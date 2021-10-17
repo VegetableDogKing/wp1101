8677bd2bce121c2d9f364ee8f65d839e8d8cc75b
VegetableDogKing <vincent97295@gmail.com>
2021-10-18 02:38:20 +0800

    push hw2

---
+++ hw2/main.js
@@ -0,0 +1,273 @@
+function numberbutton_click() {
+  var ntitle = document.getElementById("numberpicture") ;
+  var atitle = document.getElementById("alphabetpicture") ;
+  if(ntitle.style.height == "0px") {
+    ntitle.style.height = "auto" ;
+  }
+  else {
+    ntitle.style.height = "0px" ;
+  }
+  atitle.style.height = "0px" ;
+}
+
+function alphabetbutton_click() {
+  var atitle = document.getElementById("alphabetpicture") ;
+  var ntitle = document.getElementById("numberpicture") ;
+  if(atitle.style.height == "auto") {
+    atitle.style.height = "0px" ;
+  }
+  else {
+    atitle.style.height = "auto" ;
+  }
+  ntitle.style.height = "0px" ;
+}
+
+function n1_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "3px black solid" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/number/1.png" ;
+}
+
+function n2_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "3px black solid" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/number/2.png" ;
+}
+
+function n3_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "3px black solid" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/number/3.png" ;
+}
+
+function n4_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "3px black solid" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/number/4.png" ;
+}
+
+function n5_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "3px black solid" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/number/5.png" ;
+}
+
+function a_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "3px black solid" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/alphabet/a.png" ;
+}
+
+function b_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "3px black solid" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/alphabet/b.png" ;
+}
+function c_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "3px black solid" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/alphabet/c.png" ;
+}
+function d_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "3px black solid" ;
+  ae.style.border = "0px" ;
+  pic.src = "image/alphabet/d.png" ;
+}
+function e_click() {
+  var a1 = document.getElementById("number1") ;
+  var a2 = document.getElementById("number2") ;
+  var a3 = document.getElementById("number3") ;
+  var a4 = document.getElementById("number4") ;
+  var a5 = document.getElementById("number5") ;
+  var aa = document.getElementById("a") ;
+  var ab = document.getElementById("b") ;
+  var ac = document.getElementById("c") ;
+  var ad = document.getElementById("d") ;
+  var ae = document.getElementById("e") ;
+  var pic = document.getElementById("bigpicture") ;
+  a1.style.border = "0px" ;
+  a2.style.border = "0px" ;
+  a3.style.border = "0px" ;
+  a4.style.border = "0px" ;
+  a5.style.border = "0px" ;
+  aa.style.border = "0px" ;
+  ab.style.border = "0px" ;
+  ac.style.border = "0px" ;
+  ad.style.border = "0px" ;
+  ae.style.border = "3px black solid" ;
+  pic.src = "image/alphabet/e.png" ;
+}
+function empty_click() {
+  alert("這是一個空白相簿!") ;
+}
